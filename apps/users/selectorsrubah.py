7bbf8c3a2f63b3cbd448b0a19005bcec8c8b911d
LapakNgoding <lapakngoding@gmail.com>
2026-02-14 23:53:04 +0700

    Clean up and add new files

--- apps/users/selectorsrubah.py
+++
@@ -1,7 +0,0 @@
-# users/selectors.py
-def get_active_users():
-    return User.objects.filter(is_active=True)
-
-def search_users(query):
-    return User.objects.filter(username__icontains=query)
-
